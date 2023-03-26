94cc363aa70c20d6aa657307ec7de67e69118e5f
zooputer <zooputer@zoozoos-MacBook.local>
2023-03-26 22:32:19 +0900

    0326

---
+++ src/TEST/P88/Main.java
@@ -0,0 +1,79 @@
+package TEST.P88;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] map;
+    static int[][] ch;
+    static int[] dx = {-1,0,1,0,1,1,-1,-1};
+    static int[] dy = {0,1,0,-1,-1,1,1,-1};
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/TEST/P88/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        ch = new int[N][N];
+        Queue<Point> q = new LinkedList<>();
+        int cnt = 0;
+
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
+            for(int j = 0; j < N; j++){
+                map[i][j] = str.charAt(j) - '0';
+                if(map[i][j] == 1){
+                    q.add(new Point(i,j));
+                }
+            }
+        }
+
+        while(!q.isEmpty()){
+            Point cur = q.poll();
+            int flag = 0;
+
+            for(int i = 0; i < 8; i++){
+                int xx = cur.x + dx[i];
+                int yy = cur.y + dy[i];
+
+                if(0 <= xx && xx < N && 0 <= yy && yy < N &&
+                        map[xx][yy] == 1 && ch[xx][yy] == 0){
+                    ch[xx][yy] = cnt;
+                    flag = 1;
+                }
+            }
+
+            if(flag == 1){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                System.out.print(ch[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/WEEK21/P1991/Main.java
+++ src/WEEK21/P1991/Main.java
@@ -1,70 +1,101 @@
 package WEEK21.P1991;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static ArrayList<Node> tree;
-
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/WEEK21/P1991/input.txt"));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        tree = new ArrayList<>();
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            char value = st.nextToken().charAt(0);
-            int flag1 = 0;
-            int flag2 = 0;
-            char lt, rt;
+        Tree tree = new Tree();
 
-            if((lt = st.nextToken().charAt(0)) != '.'){
-                flag1 = 1;
-            }
-            if((rt = st.nextToken().charAt(0)) != '.'){
-                flag2 = 1;
-            }
-
-            if(flag1 == 0 && flag2 == 0){
-                tree.add(new Node(value, null, null));
-            } else if (flag1 == 0 && flag2 == 1) {
-                tree.add(new Node(value, null, new Node(rt, null, null)));
-            } else if (flag1 == 1 && flag2 == 0) {
-                tree.add(new Node(value, new Node(lt, null, null), null));
-            } else {
-                tree.add(new Node(value, new Node(lt, null, null), new Node(rt, null, null)));
-            }
+        for(int i = 0; i < N; i++) {
+            char[] data;
+            data = br.readLine().replaceAll(" ", "").toCharArray();
+            tree.createNode(data[0], data[1], data[2]);
         }
 
-        preOrder(tree);
+        //전위
+        tree.preorder(tree.root);
         System.out.println();
-        //inOrder(head);
+
+        //중위
+        tree.inorder(tree.root);
         System.out.println();
-        //postOrder(head);
-        System.out.println();
+
+        //후위
+        tree.postorder(tree.root);
+
+        br.close();
     }
 
-    public static void preOrder(ArrayList tree) {
-        Node root = tree.get(0);
-    }
 }
 
-class Node{
-    char value;
+class Node {
+    char data;
     Node left;
     Node right;
 
-    public Node(char value, Node left, Node right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
+    Node(char data) {
+        this.data = data;
     }
+}
+
+class Tree {
+    Node root; //루트 노드 : 처음엔 null
+
+    public void createNode(char data, char leftData, char rightData) {
+        if(root == null) { //아무것도 없는 초기 상태 - A 루트 노드 생성
+            root = new Node(data);
+
+            //좌우 값이 있는 경우에만! 노드 생성
+            if(leftData != '.') {
+                root.left = new Node(leftData);
+            }
+            if(rightData != '.') {
+                root.right = new Node(rightData);
+            }
+        } else { //초기상태가 아니면 어디에 들어가야할지 찾아야함 - A 루트 노드 이후
+            searchNode(root, data, leftData, rightData);
+        }
+    }
+
+    //여기에서 root는 매개 변수로 들어온 로컬변수 root임을 주의
+    public void searchNode(Node root, char data, char leftData, char rightData) {
+        if(root == null) { //도착한 노드가 null이면 재귀 종료 - 찾을(삽입할) 노드 X
+            return;
+        } else if(root.data == data) { //들어갈 위치를 찾았다면
+            if(leftData != '.') { //.이 아니라 값이 있는 경우에만 좌우 노드 생성
+                root.left = new Node(leftData);
+            }
+            if(rightData != '.') {
+                root.right = new Node(rightData);
+            }
+        } else { //아직 찾지못했고 탐색할 노드가 남아 있다면
+            searchNode(root.left, data, leftData, rightData); //왼쪽 재귀 탐색
+            searchNode(root.right, data, leftData, rightData); //오른쪽 재귀 탐색
+        }
+    }
+
+    // 전위순회 : 루트 -> 좌 -> 우
+    public void preorder(Node root){
+        System.out.print(root.data);                //먼저 가운데 출력
+        if(root.left!=null) preorder(root.left);    //그 다음 왼쪽
+        if(root.right!=null) preorder(root.right);   //마지막 오른쪽
+    }
+
+    // 중위순회 : 좌 -> 루트 -> 우
+    public void inorder(Node root){
+        if(root.left!=null) inorder(root.left);     //왼쪽 먼저
+        System.out.print(root.data);                //그 다음 중앙 출력
+        if(root.right!=null) inorder(root.right);    //마지막으로 오른쪽
+    }
+
+    // 후위순회 : 좌 -> 우 -> 루트
+    public void postorder(Node root){
+        if(root.left!=null) postorder(root.left);    //왼쪽 먼저
+        if(root.right!=null) postorder(root.right);     //그 다음 오른쪽
+        System.out.print(root.data);
+    }
+
 }
