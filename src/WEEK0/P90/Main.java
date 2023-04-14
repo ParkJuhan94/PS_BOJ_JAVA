f3a67d7c35c6b32b7f2331789dd25a6e77ff7901
ParkJuhan <1004sunye@naver.com>
2023-04-14 21:18:29 +0900

    0414

--- src/WEEK0/P90/Main.java
+++ src/WEEK0/P90/Main.java
@@ -7,10 +7,6 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int N;
-    static int M;
-    static int[][] map;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, -1, 0, 1};
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/WEEK0/P90/input.txt"));
