8169c3210da1154c1f2af4da6c3e8732de25d885
ParkJuhan <1004sunye@naver.com>
2023-04-20 20:35:47 +0900

    0420

---
+++ src/WEEK0/P1094/Main.java
@@ -0,0 +1,20 @@
+package WEEK0.P1094;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/WEEK0/P1094/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(br.readLine());
+    }
+
+}
