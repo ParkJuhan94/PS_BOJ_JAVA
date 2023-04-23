d2c259b8b4caf1e1b7860a3d587eaa2de2e1f793
ParkJuHan94 <1004sunye@naver.com>
2023-04-24 04:00:30 +0900

    0424

--- src/WEEK0/P5430/Main.java
+++ src/WEEK0/P5430/Main.java
@@ -37,27 +37,52 @@ public class Main {
 
             // 연산 시작
             boolean reverse = false;
+            int startIdx = 0;
+            int endIdx = arr.size() - 1;
+
             for(int i = 0; i < op.length(); i++){
                 if(op.charAt(i) == 'R'){
                     // 실제로 뒤집는게 아니라, 방향 변수 reverse 만 역전시켜주고 연산 진행
                     reverse = (reverse == true) ? false : true;
-
                 } else if (op.charAt(i) == 'D'){
                     // 원소가 하나도 없으면 에러 출력
+                    if(startIdx > endIdx){
+                        System.out.println("error");
+                        break;
+                    }
 
-                    // 
+                    // 원소가 하나라도 있으면 삭제
+                    // 실제로 remove 를 하면 시간초과!!!
                     if(reverse){
-
+                        //arr.remove(arr.size() - 1);
+                        endIdx--;
                     }else{
-
+                        //arr.remove(0);
+                        startIdx++;
                     }
                 }
-
-                // 출력
-
             }
 
-            System.out.println();
+            // 출력
+            if(startIdx < endIdx){
+                System.out.print("[");
+                if(reverse){
+                    for(int i = endIdx; i >= startIdx; i--){
+                        System.out.print(arr.get(i));
+                        if(i != startIdx){
+                            System.out.print(",");
+                        }
+                    }
+                }else{
+                    for(int i = startIdx; i <= endIdx; i++){
+                        System.out.print(arr.get(i));
+                        if(i != endIdx){
+                            System.out.print(",");
+                        }
+                    }
+                }
+                System.out.println("]");
+            }
         }
     }
 
