ce5de010c17700ddf80c794ac79ff6d4f30ce32a
ParkJuHan94 <1004sunye@naver.com>
2023-04-26 22:26:15 +0900

    0426

--- src/WEEK0/P14500/Main.java
+++ src/WEEK0/P14500/Main.java
@@ -10,6 +10,10 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, M;
     static int[][] map;
+    static boolean[][] ch;
+    static int ans;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/WEEK0/P14500/input.txt"));
@@ -17,9 +21,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
+        ch = new boolean[N][M];
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0 ; j < M; j++){
@@ -27,27 +32,43 @@ public class Main {
             }
         }
 
-        int ans = 0;
+        ans = 0;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
-               if(search(i, j) > ans){
-                   ans = search(i, j);
-               }
+                ch[i][j] = true;
+                search(i, j, 1, map[i][j]);
+                ch[i][j] = false;
             }
         }
 
-        System.out.println(ans);
+        System.out.print(ans);
     }
 
-    // x행 y열에서 시작하는 테트로미노 15가지 중에서(회전 포함) 최대의 합을 return
-    static int search(int x, int y){
-        int max = 0;
-
-        for(){
-
+    // 완전탐색 : 해당 점을 포함하는 모든 테트로미노를 구함
+    static void search(int x, int y, int cnt, int sum){
+        if(cnt == 4){
+            ans = Math.max(sum, ans);
+            return;
         }
 
-        return max;
+        for(int i = 0; i < 4; i++){
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+
+            if(0 <= xx && xx < N && 0 <= yy && yy < M
+                    && !ch[xx][yy]){
+                // 분홍색(ㅜ) 테트로미노 만들기 위해 2번째 칸에서 탐색 한번 더 진행
+                if(cnt == 2){
+                    ch[xx][yy] = true;
+                    search(x, y, cnt + 1, sum + map[xx][yy]);
+                    ch[xx][yy] = false;
+                }
+
+                ch[xx][yy] = true;
+                search(xx, yy, cnt + 1, sum + map[xx][yy]);
+                ch[xx][yy] = false;
+            }
+        }
     }
 
 }
