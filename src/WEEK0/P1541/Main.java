b739cde3bf546538477e3c2109871962df44eef2
ParkJuHan94 <1004sunye@naver.com>
2023-05-01 16:27:29 +0900

    0501

--- src/WEEK0/P1541/Main.java
+++ src/WEEK0/P1541/Main.java
@@ -4,18 +4,30 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
+    static ArrayList<Integer> arr;
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/WEEK0/P/input.txt"));
+        System.setIn(new FileInputStream("src/WEEK0/P1541/input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-+");
 
-        N = Integer.parseInt(st.nextToken());
+        arr = new ArrayList<>();
+        while(st.hasMoreTokens()){
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+
+//        for(int i = 0; i < arr.size(); i++){
+//            System.out.println(arr.get(i));
+//        }
+
+        
     }
 
 }
