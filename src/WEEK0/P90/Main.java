111b44ea06f52755089a69868b44d02788409a0e
ParkJuHan94 <1004sunye@naver.com>
2023-05-03 16:24:43 +0900

    0503

--- src/WEEK0/P90/Main.java
+++ src/WEEK0/P90/Main.java
@@ -7,24 +7,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static String N;
-    static String M;
+    static int N;
+    static int M;
 
     public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/WEEK0/P90/input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        //StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine()," ");
-        N = st.nextToken();
-        M = st.nextToken();
+        //N = Integer.parseInt(st.nextToken());
 
-        String ans = "";
-        int len = N.length();
-        for(int i = 0; i < len; i++){
-
+        int max = 0;
+        int maxIdx = -1;
+        for(int i = 0; i < 9; i++){
+            int input = Integer.parseInt(br.readLine());
+            if(input > max){
+                max = Math.max(max, input);
+                maxIdx = i;
+            }
         }
 
+        System.out.println(max);
+        System.out.println(maxIdx + 1);
+
+//        int ans = 0;
+//
+//
+//        System.out.println(ans);
     }
 
 }
