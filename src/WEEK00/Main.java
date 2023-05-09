59a2422efb2e4715a9cafe82748cd3d4a24339ea
ParkJuHan94 <1004sunye@naver.com>
2023-05-09 23:56:11 +0900

    0509

--- src/WEEK00/Main.java
+++ src/WEEK00/Main.java
@@ -10,12 +10,13 @@ public class Main {
     static int N;
     static int M;
     static int[][] map;
+    static int[][] ch;
     static int[] dr = {0, 1, 0, -1};
     static int[] dc = {1, 0, -1, 0};
     static int ans = 0;
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/WEEK0/P/input.txt"));
+        System.setIn(new FileInputStream("src/WEEK00/P/input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
