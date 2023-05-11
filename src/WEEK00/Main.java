9556082d07705049268c4c22ef05663d11096c6c
ParkJuHan94 <1004sunye@naver.com>
2023-05-12 01:24:24 +0900

    0512

--- src/WEEK00/Main.java
+++ src/WEEK00/Main.java
@@ -14,14 +14,24 @@ public class Main {
     static int[] dr = {0, 1, 0, -1};
     static int[] dc = {1, 0, -1, 0};
     static int ans = 0;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/WEEK00/P/input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
+
+
+
+
+
+
+
+        System.out.print(sb);
     }
 
 }
