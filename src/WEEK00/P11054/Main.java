6b90bddd3b04b80b353c13399fcab0b05190406a
ParkJuHan94 <1004sunye@naver.com>
2023-05-27 07:11:17 +0900

    0527

--- src/WEEK00/P11054/Main.java
+++ src/WEEK00/P11054/Main.java
@@ -14,6 +14,8 @@ public class Main {
     static int[] dr = {0, 1, 0, -1};
     static int[] dc = {1, 0, -1, 0};
     static int[] dp;
+    static int[] dp1;
+    static int[] dp2;
     static int ans = 0;
     static StringBuilder sb;
 
@@ -31,19 +33,35 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        //dp[i]는 수열의 i번째 원소를 마지막으로 하는 가장 긴 바이토닉 수열의 길이
-        dp = new int[N];
-        dp[0] = 1;
+        int[] dp1 = new int[N]; // 증가하는 부분 수열의 길이를 저장하는 배열
+        int[] dp2 = new int[N]; // 감소하는 부분 수열의 길이를 저장하는 배열
+        int[] dp = new int[N]; // 바이토닉 부분 수열의 길이를 저장하는 배열
 
-        for (int i = 1; i < N; i++) {
-            dp[i] = 1; // 각 원소의 길이는 최소 1로 초기화
+        // 증가
+        for (int i = 0; i < N; i++) {
+            dp1[i] = 1; // 각 원소의 길이는 최소 1로 초기화
             for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1; // 증가하는 순서를 만족하고 더 긴 길이라면 업데이트
+                if (arr[j] < arr[i] && dp1[j] + 1 > dp1[i]) {
+                    dp1[i] = dp1[j] + 1; // 증가하는 순서를 만족하고 더 긴 길이라면 업데이트
                 }
             }
         }
 
+        // 감소
+        for (int i = N - 1; i >= 0; i--) {
+            dp2[i] = 1; // 각 원소의 길이는 최소 1로 초기화
+            for (int j = N - 1; j > i; j--) {
+                if (arr[j] < arr[i] && dp2[j] + 1 > dp2[i]) {
+                    dp2[i] = dp2[j] + 1; // 감소하는 순서를 만족하고 더 긴 길이라면 업데이트
+                }
+            }
+        }
+
+
+        // 바이토닉
+        for(int i = 0; i < N; i++){
+            dp[i] = dp1[i] + dp2[i] - 1;
+        }
 
         int maxLength = 0;
         for (int i = 0; i < N; i++) {
