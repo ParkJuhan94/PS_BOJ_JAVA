1c0334e6dbd3d4462afb1c2230182584041cbb00
ParkJuHan94 <1004sunye@naver.com>
2023-05-27 23:40:40 +0900

    0527

--- src/WEEK00/P11054/Main.java
+++ src/WEEK00/P11054/Main.java
@@ -38,6 +38,7 @@ public class Main {
         int[] dp = new int[N]; // 바이토닉 부분 수열의 길이를 저장하는 배열
 
         // 증가
+        // dp1[i]는 수열의 i번째 원소를 마지막으로 하는 증가하는 부분 수열의 최대 길이
         for (int i = 0; i < N; i++) {
             dp1[i] = 1; // 각 원소의 길이는 최소 1로 초기화
             for (int j = 0; j < i; j++) {
@@ -48,6 +49,7 @@ public class Main {
         }
 
         // 감소
+        // dp2[i]는 수열의 i번째 원소를 시작으로 하는 감소하는 부분 수열의 최대 길이
         for (int i = N - 1; i >= 0; i--) {
             dp2[i] = 1; // 각 원소의 길이는 최소 1로 초기화
             for (int j = N - 1; j > i; j--) {
@@ -59,6 +61,7 @@ public class Main {
 
 
         // 바이토닉
+        // 정수 i가 2번 카운팅되니까 -1 해준다
         for(int i = 0; i < N; i++){
             dp[i] = dp1[i] + dp2[i] - 1;
         }
