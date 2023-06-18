91645995db669d5e4b5abc8cb3458e1e5538a00f
ParkJuHan94 <1004sunye@naver.com>
2023-06-18 23:27:24 +0900

    0618

---
+++ src/WEEK000/P11000/Main.java
@@ -0,0 +1,60 @@
+package WEEK000.P11000;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[][] map;
+    static int[][] ch;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+    static int ans = 0;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/WEEK000/P11000/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        Lesson[] lessons = new Lesson[N];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            long s = Integer.parseInt(st.nextToken());
+            long e = Integer.parseInt(st.nextToken());
+
+            lessons[i] = new Lesson(s, e);
+        }
+
+
+
+
+
+
+
+        System.out.print(sb);
+        //System.out.println(ans);
+    }
+
+    static class Lesson implements Comparable<Lesson>{
+        long start;
+        long end;
+
+        public Lesson(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Lesson o) {
+            return 0;
+        }
+    }
+}
